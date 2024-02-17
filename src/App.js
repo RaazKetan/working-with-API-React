03b2248322811aab2052aedf5767357d11bd04bd
Raaz <21ketanraaz@gmail.com>
2024-02-17 12:35:50 +0530

    Working with loading phase

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   
+
  async function fetchMoviesHandler() {
+  setIsLoading(true);
  const response=  await fetch('https://swapi.dev/api/films/')
  const data = await response.json();
  
@@ -18,7 +21,8 @@ function App() {
             releaseDate: movieData.release_date
           };
         });
-        setMovies(transformedMovies); 
+        setMovies(transformedMovies);
+        setIsLoading(false); 
   }
 
   return (
@@ -27,7 +31,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+      {!isLoading && movies.length > 0 &&
+        <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies found.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );

e3f0cdc3e0ad0889faff11d8e71ba67ad2004952
Raaz <21ketanraaz@gmail.com>
2024-02-17 14:11:30 +0530

    Addign Add Movie Section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState,useEffect,useCallback} from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-
-
-  const fetchMoviesHandler = useCallback(async()=>{
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    try{ 
-      const response = await fetch("https://swapi.dev/api/films/");
-      
-      if(!response.ok){
-        throw new Error('Something went wrong');
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
       }
+
       const data = await response.json();
+
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -29,37 +29,43 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-    }
-   catch(error){
-     setError(error.message);
+    } catch (error) {
+      setError(error.message);
     }
     setIsLoading(false);
-  },[]);
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
-  if(movies.length > 0){
+  if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
-  if(error){
-    content = <p>{error}</p>
+
+  if (error) {
+    content = <p>{error}</p>;
   }
-  if(isLoading){
+
+  if (isLoading) {
     content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
